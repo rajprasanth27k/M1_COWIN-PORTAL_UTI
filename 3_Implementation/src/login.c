510626d3c6e9da5772870c93c1a9abb193cd84a8
rajprasanth27k <73667634+rajprasanth27k@users.noreply.github.com>
2022-03-03 11:37:38 +0530

    Add files via upload

---
+++ 3_Implementation/src/login.c
@@ -0,0 +1,32 @@
+#include "head.h"
+
+
+
+
+int login(int a)
+{
+    for(int i=0;i<100;i++)
+    {
+      if(a==1234)
+        return 1;
+      else
+        return 0;
+     
+    }  
+	
+}
+int second(int v1)
+{
+    if(v1==1)
+    {
+         return 1;
+        
+    }
+    else
+    {
+         return 2;
+    }
+    
+
+
+}
---
+++ 3_Implementation/src/prov.c
@@ -0,0 +1,29 @@
+ #include "head.h"
+
+int prov(char *pov)
+{
+        if(pov=="cipla"||"mediplus" || "appllo")
+        {
+            return 1;
+                
+        }
+        else
+        {
+            return 0;
+        }
+}
+
+int Ddose(int dose)
+{
+
+         if(dose==1)
+        {
+            return 1;
+        }
+        else if(dose==2)
+        {
+            return 2;
+        } 
+
+}
+
